f4ea92e2aa4146e575ed6deb4ce1782f91c90b6a
Holt Mansfield <holto@Holts-MBP-2.ht.home>
2023-01-01 20:16:30 -0500

    adding mongo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
-import express  from 'express'
+import express from 'express'
+import { connectMongo } from './mongo/setup'
 import { getMiddleware } from './graphql/setup'
 
-var app = express()
+const run = async () => {
+  await connectMongo()
+  const app = express()
 
-// add GraphQL endpoint
-app.use('/graphql', getMiddleware());
+  // add GraphQL endpoint
+  app.use('/graphql', getMiddleware())
 
+  app.listen(4000)
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql')
+}
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql')
+run()
