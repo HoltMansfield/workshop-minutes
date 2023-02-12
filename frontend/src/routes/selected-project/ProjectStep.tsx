1161a96e2659848a20ff7c12c1986a41bc31bfbe
Holt Mansfield <holtmansfield@gmail.com>
2023-02-11 19:13:54 -0500

    switched to pill and delete step

--- frontend/src/routes/selected-project/ProjectStep.tsx
+++ frontend/src/routes/selected-project/ProjectStep.tsx
@@ -1,13 +1,16 @@
-import { Box, Grid, IconButton, Paper } from "@mui/material"
+import { Box, Chip, Grid, IconButton, ListItemIcon, ListItemText, MenuItem, Paper } from "@mui/material"
 import { grey } from "@mui/material/colors"
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled'
 import StopCircleIcon from '@mui/icons-material/StopCircle'
-import SkipNextIcon from '@mui/icons-material/SkipNext'
+import DeleteIcon from '@mui/icons-material/Delete'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import SwipeIcon from '@mui/icons-material/Swipe'
 import { Step } from "src/DMS/collections/project"
 import { useState } from "react"
 import { useProjectState } from "src/hooks/state/useProjectState"
 import { useUpdateProject } from "src/DMS/hooks/api/collections/project/useUpdateProject"
 import { useToaster } from "src/hooks/useToaster"
+import { MeatBallMenu } from "src/app/components/MeatballMenu"
 
 interface ProjectStepProps {
   step: Step
@@ -15,7 +18,7 @@ interface ProjectStepProps {
 
 const _getMinutesLabel = (secondsElapsed: Number) => {
   if (!secondsElapsed) {
-    return '(Not Started)'
+    return 'Not Started'
   }
 
   const minutesLabel = Number(secondsElapsed) / 60
@@ -26,7 +29,7 @@ const _getMinutesLabel = (secondsElapsed: Number) => {
     secondsLabel = `0${secondsLabel}`
   }
 
-  return `(${Math.floor(minutesLabel)}:${secondsLabel})`
+  return `${Math.floor(minutesLabel)}:${secondsLabel}`
 }
 
 export const ProjectStep = ({ step }: ProjectStepProps) => {
@@ -70,16 +73,63 @@ export const ProjectStep = ({ step }: ProjectStepProps) => {
     })
   }
 
+  const handleDelete = () => {
+    const updatableSteps = selectedProject.steps.filter(s => s.name !== step.name)
+    const updateRequest = {
+      query: { _id: { $oid: selectedProject._id  } },
+      update: {
+        "$set": {
+          steps: updatableSteps
+        }
+      }
+    }
+
+    mutation.mutate(updateRequest, {
+      onSuccess: (data) => {
+        setSelectedProject({...selectedProject, steps: updatableSteps })
+      },
+      onError: displayMutationError
+    })
+  }
+
+  const handleSetTimer = () => {
+
+  }
+
+  const items = [
+    <MenuItem key="delete" onClick={handleDelete}>
+      <ListItemIcon>
+        <DeleteIcon fontSize="small" />
+      </ListItemIcon>
+      <ListItemText>Delete</ListItemText>
+    </MenuItem>,
+    <MenuItem key="set-timer" onClick={handleSetTimer}>
+      <ListItemIcon>
+        <AccessTimeIcon fontSize="small" />
+      </ListItemIcon>
+      <ListItemText>Set Timer</ListItemText>
+    </MenuItem>,
+    <MenuItem key="manual-override">
+      <ListItemIcon>
+        <SwipeIcon fontSize="small" />
+      </ListItemIcon>
+      <ListItemText>Manually enter time</ListItemText>
+    </MenuItem>
+  ]
+
   return (
     <Paper sx={{ display: 'flex', flexGrow: 1, backgroundColor: grey[200] }} variant="outlined">
       <Grid container spacing={2}>
         <Grid item xs={12} md={3}>
           <Box display="flex" m={1}>
-            <Box display="flex">
+            <Box display="flex" fontSize="0.9rem" fontWeight="bold" marginTop={0.2}>
+              <Chip label={_getMinutesLabel(seconds)} />
+            </Box>
+            <Box display="flex" mt={0.8} ml={1}>
               {step.name}
             </Box>
-            <Box display="flex" ml={1} fontSize="0.9rem" fontWeight="bold" marginTop={0.2}>
-              {_getMinutesLabel(seconds)}
+            <Box display="flex" marginLeft="auto">
+              <MeatBallMenu items={items} />
             </Box>
           </Box>
         </Grid>
@@ -90,9 +140,6 @@ export const ProjectStep = ({ step }: ProjectStepProps) => {
             </Box>
             <Box display="flex" onClick={handleStop}>
               <IconButton><StopCircleIcon /></IconButton>
-            </Box>
-            <Box display="flex">
-              <IconButton><SkipNextIcon /></IconButton>
             </Box>
           </Box>
         </Grid>
